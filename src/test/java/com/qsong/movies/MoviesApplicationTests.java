62fd3a772a94af41fe1941e4733bde68e447f347
Junyi <qsong.vip@qq.com>
2020-05-03 09:24:44 +0800

    [Junyi] Init project

---
+++ src/test/java/com/qsong/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsong.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
